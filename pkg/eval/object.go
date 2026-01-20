ba26ac5346cd92fd8ca216a18eaf674a85053859
dev-dami <dev-dami@users.noreply.github.com>
2026-01-20 04:58:53 +0100

    feat(eval): add runtime object types
    - Integer, Float, String, Boolean objects
    - Array and Function objects
    - Error and Nil objects
    - Return value wrapper

---
+++ pkg/eval/object.go
@@ -0,0 +1,148 @@
+package eval
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/carv-lang/carv/pkg/ast"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ      ObjectType = "INTEGER"
+	FLOAT_OBJ        ObjectType = "FLOAT"
+	BOOLEAN_OBJ      ObjectType = "BOOLEAN"
+	STRING_OBJ       ObjectType = "STRING"
+	CHAR_OBJ         ObjectType = "CHAR"
+	NIL_OBJ          ObjectType = "NIL"
+	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
+	ERROR_OBJ        ObjectType = "ERROR"
+	FUNCTION_OBJ     ObjectType = "FUNCTION"
+	BUILTIN_OBJ      ObjectType = "BUILTIN"
+	ARRAY_OBJ        ObjectType = "ARRAY"
+	BREAK_OBJ        ObjectType = "BREAK"
+	CONTINUE_OBJ     ObjectType = "CONTINUE"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Float struct {
+	Value float64
+}
+
+func (f *Float) Type() ObjectType { return FLOAT_OBJ }
+func (f *Float) Inspect() string  { return fmt.Sprintf("%g", f.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
+
+type Char struct {
+	Value rune
+}
+
+func (c *Char) Type() ObjectType { return CHAR_OBJ }
+func (c *Char) Inspect() string  { return string(c.Value) }
+
+type Nil struct{}
+
+func (n *Nil) Type() ObjectType { return NIL_OBJ }
+func (n *Nil) Inspect() string  { return "nil" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+	Line    int
+	Column  int
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string {
+	if e.Line > 0 {
+		return fmt.Sprintf("error at %d:%d: %s", e.Line, e.Column, e.Message)
+	}
+	return fmt.Sprintf("error: %s", e.Message)
+}
+
+type Function struct {
+	Parameters []*ast.Parameter
+	Body       *ast.BlockStatement
+	Env        *Environment
+	Name       string
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var params []string
+	for _, p := range f.Parameters {
+		params = append(params, p.Name.Value)
+	}
+	return fmt.Sprintf("fn(%s) { ... }", strings.Join(params, ", "))
+}
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (a *Array) Type() ObjectType { return ARRAY_OBJ }
+func (a *Array) Inspect() string {
+	var elements []string
+	for _, e := range a.Elements {
+		elements = append(elements, e.Inspect())
+	}
+	return "[" + strings.Join(elements, ", ") + "]"
+}
+
+type Break struct{}
+
+func (b *Break) Type() ObjectType { return BREAK_OBJ }
+func (b *Break) Inspect() string  { return "break" }
+
+type Continue struct{}
+
+func (c *Continue) Type() ObjectType { return CONTINUE_OBJ }
+func (c *Continue) Inspect() string  { return "continue" }
+
+var (
+	NIL   = &Nil{}
+	TRUE  = &Boolean{Value: true}
+	FALSE = &Boolean{Value: false}
+	BREAK = &Break{}
+	CONT  = &Continue{}
+)
